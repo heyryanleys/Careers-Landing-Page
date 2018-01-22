137fd92877c438897eff92207db17825b00c0a33
Ryan Leys <heyryanleys@gmail.com>
2018-01-22 12:27:00 -0500

    Adding rough styling for new tradeshow page

---
+++ Tradeshows/js.js
@@ -0,0 +1,11 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>
+  window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')
+</script>
+<script>
+$(document).ready(function(){
+    $("span[id*=section_2]+.einspaltigG div img").attr("src", "https://s14.postimg.org/3k919u1vl/bannerfortradeshowpage.png");
+  }
+ )
+
+</script>
